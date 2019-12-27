689d2e59294b917d002b65c191e50c7aa2c57bc5
RagingDenny <mgjoslin1@gmail.com>
2019-12-27 11:09:44 -0500

    Update GamePlay.py

--- GamePlay.py
+++ GamePlay.py
@@ -1,11 +1,10 @@
 #Main code for the game loop
-#RPG type game, similar to D&D
+#RPG type game, similar to D&D but set in teh world of Thea during the Machina wars
 
-#Game layout: Splash text
-#             Continue or start new?
-#               Classes: Soldier, technician, smuggler
-#             Load or create character
-#             Go through game loop
+################
+### Includes ###
+################
+#include "GameModules.h"
 
 ###############
 ## Variables ##
@@ -13,25 +12,32 @@
 
 gameOver = 0
 stop = 0
+validEntry = 0
+characterName = ""
+characterClass = ""
+strength = ""
+intelligence = ""
+charisma = ""
+dexterity = ""
+wisdom = ""
 
 #Function Defs
 def createCharacter():
     #Player Character values
-    characterName = ""
-    characterClass = ""
-    strength = ""
-    intelligence = ""
-    charisma = ""
-    dexterity = ""
-    wisdom = ""
+    global characterName, characterClass, strength, intelligence, charisma, dexterity, wisdom
     pointSum = 0
-    
-    print("Code to create a character to go here")
-    print("Enter Character Name")
-    charaterName = input()
+
     print("Choose Character Class: 1: 2: 3: ")
     characterClass = input()
-
+    
+    while (characterName == ""):
+        print("Enter Character Name")
+        characterName = input()
+        if len(characterName) == 0 or len(characterName) > 10:
+            print("Character Name is empty or greater than 10 characters")
+            characterName = ""
+            continue
+        
     while (pointSum !=5):
         #User distributes skill points
         print("Distribute your 5 points")
@@ -79,26 +85,42 @@ def createCharacter():
             continue
             pointSum = 0
         pointSum += int(wisdom)
-        
+
+        if pointSum != 5:
+            print("Points not distributed properly")
+            pointSum = 0
+            continue
+
+    f = open("player", "w")
+    f.write(characterName + "\n")
+    f.write("Strength: " + strength + "\n")
+    f.write("Intelligence: " + intelligence + "\n")
+    f.write("Charisma: " + charisma + "\n")
+    f.write("Dexterity: " + dexterity + "\n")
+    f.write("Wisdom: " + wisdom)
+    #f.close()
+    
 
 def loadCharacter():
     print("Code to load character goes here")
-
-#Splash text
-print("Welcome to the world of Thea");
-print("Enter 1 for new game or 2 to continue")
-start = input()
-
-if start == "1":
-    createCharacter()
-elif start == "2":
-    loadCharacter()
-else:
-    print("Invalid entry")
-        
-
-
-
-
-#while(gameOver):
-    #Run game until game over or player quits
+    global characterName, characterClass, strength, intelligence, charisma, dexterity, wisdom
+    
+def splashScreen():
+    global validEntry
+    print("\t Welcome to the world of Thea");
+    while(not validEntry):
+        print("\t Enter 1 for new game or 2 to continue")
+        start = input()
+        if start == "1":
+            validEntry = 1
+            createCharacter()
+        elif start == "2":
+            validEntry = 1
+            loadCharacter()
+        else:
+            print("Invalid Entry")
+            
+while(not gameOver):
+    #Main game loop code
+    splashScreen()
+    exit()
